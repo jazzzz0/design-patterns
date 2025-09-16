e824b6e20d0838060de1e95a0cc2fffc970fc53c
jazzzz0 <maezjazmin@gmail.com>
2025-09-16 02:43:38 -0300

    Initial commit of project files

---
+++ README.md
@@ -0,0 +1,43 @@
+## Práctica de patrones de diseño
+Los patrones a practicar en principio son:
+- Creacionales:
+    - Factory,
+    - Singleton,
+    - Builder.
+- Comportamiento:
+    - Command,
+    - Observer,
+    - Strategy.
+- Estructurales:
+    - Adapter,
+    - Composite.
+
+### Ejercicios hechos
+#### Adapter
+- **Adaptador de servicios de almacenamiento**. 
+<br> Tenemos una interfaz ISubidorFoto que expone un contrato a toda clase que la implemente, éstas deberán implementar el método subir_foto(). Las clases que implementan la interfaz serán **Adapter** de un servicio.
+<br> Se simula un servidor que recibe una petición POST para guardar una imagen. La práctica se basa en almacenar la misma en el servicio determinado por la configuración del usuario. Dependiendo de la configuración, se instanciará el Adapter adecuado. 
+<br> El uso de **Adapter** y la definición del método **publicar_foto_en_nube()** permite intercambiar objetos sin modificar el código que los utiliza. Sin importar si se usa `DropboxAdapter` o `GoogleDriveAdapter`, el código siempe llama a la misma cosa: **foto_uploader.subir_foto()**
+
+```
+
+    public String publicar_foto_en_nube(String foto, String storage){
+        ISubidorFoto foto_uploader = null;
+
+        if (storage=="gdrive"){
+            foto_uploader = new GoogleDriveAdapter();
+        }
+        if (storage=="dropbox"){
+            foto_uploader = new DropboxAdapter();
+        } 
+        if (foto_uploader == null){
+            return new Exception().toString();
+        }
+
+        return foto_uploader.subir_foto(foto);
+    }
+
+```
+
+<br> El beneficio de este patron es que aísla el lugar donde se decide que tipo de objeto usar. El codigo que realmente hace el trabajo (`subir_foto`) no necesita saber si esta trabajando con Dropbox o Google Drive.
+<br> La clave está en que si tuviera que añadir 10 servicios de almacenamiento más, solo tendría que añadir unos `if` o un `switch`. Pero el código que llama al método `subir_foto()` seguiría siendo el mismo, una sola línea. Esto simplifica el **mantenimiento del código a largo plazo** en un proyecto.
---
+++ adapter/almacenamiento/Main.java
@@ -0,0 +1,104 @@
+package adapter.almacenamiento;
+public class Main {
+    public static void main(String[] args) {
+        String foto = "foto.jpg";
+        User user = new User("gdriv");
+        String storage_config = user.get_storage_config();
+
+        String request = "POST www.api.com/api/posts/\n-------Headers-------\nAuthorization: Bearer ajcofjdv.swier3nflj3.dfg\n-------Body-------\n{'image':'"+ foto +"'}";
+
+        PostView APIview = new PostView();
+        System.out.println(APIview.post(request, foto, storage_config));
+    }
+
+}
+
+class User {
+    String storage_config;
+    public User(String storage_selected){
+        if (storage_selected == "gdrive" || storage_selected == "dropbox") {
+            this.storage_config = storage_selected;
+        } else {
+            this.storage_config = "onedrive";
+        }
+    }
+    String get_storage_config(){
+        return this.storage_config;
+    }
+}
+
+class PostView{
+    public String post(String request, String foto, String storage){
+        try {
+            String response = publicar_foto_en_nube(foto, storage);
+            return "Response: {\n'success': True, \n'data':" + response + "\n}\nStatus code: 200 OK";
+        } catch (Exception e) {
+            return "Response: {\n'success': False, \n'message': " + e.toString() + "\n}\nStatus code: 400 BAD REQUEST";
+        }
+    }
+
+    public String publicar_foto_en_nube(String foto, String storage){
+        ISubidorFoto foto_uploader = null;
+        if (storage=="gdrive"){
+            foto_uploader = new GoogleDriveAdapter();
+        }
+        if (storage=="dropbox"){
+            foto_uploader = new DropboxAdapter();
+        } 
+        if (foto_uploader == null){
+            return new Exception().toString();
+        }
+
+        return foto_uploader.subir_foto(foto);
+    }
+
+    // ESTA FORMA ESTARÍA MAL YA QUE REPITE CÓDIGO (los return)
+    // public String publicar_foto_en_nube(String foto, String storage){
+    //     if (storage == "gdrive"){
+    //         GoogleDriveAdapter gdriveAdapter = new GoogleDriveAdapter();
+    //         return gdriveAdapter.subir_foto(foto);
+    //     }
+    //     if (storage == "dropbox"){
+    //         DropboxAdapter dropboxAdapter = new DropboxAdapter();
+    //         return dropboxAdapter.subir_foto(foto);
+    //     }
+    //     return new Exception().toString();
+    // }
+
+}
+
+// ----------------- SERVICIOS -----------------
+class Dropbox {
+    public String upload_to_dropbox(String foto){
+        return foto + " se ha subido a www.dropbox.com/posts/"+ foto;
+    }
+}
+
+class Drive {
+    public String send_to_gdrive(String foto){
+        return foto + " se ha subido a www.drive.google.com/posts/"+ foto;
+    }
+}
+
+
+// ----------------- INTERFAZ P/ ADAPTERS -----------------
+interface ISubidorFoto {
+    public String subir_foto(String foto);
+}
+
+// ----------------- ADAPTERS  -----------------
+
+class DropboxAdapter implements ISubidorFoto {
+    Dropbox adaptee = new Dropbox();
+    public String subir_foto(String foto){
+        return adaptee.upload_to_dropbox(foto);
+    }
+}
+
+class GoogleDriveAdapter implements ISubidorFoto {
+    Drive adaptee = new Drive();
+    public String subir_foto(String foto){
+        return adaptee.send_to_gdrive(foto);
+    }
+}
+
